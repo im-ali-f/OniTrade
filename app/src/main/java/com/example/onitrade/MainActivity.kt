0fdd442a27ca6a3afd9915b9df015d24a74f3ac5
unknown <alifarhad.work@gmail.com>
2024-02-20 16:41:38 +0330

    first Commit

---
+++ app/src/main/java/com/example/onitrade/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.onitrade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.onitrade.ui.theme.OniTradeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/onitrade/ui/theme/Color.kt
@@ -0,0 +1,35 @@
+package com.example.onitrade.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+
+
+val iconColor = Color(0xFFA3A5AC)
+val mainRedColor = Color(0xFFFF2C7B)
+val mainGreenColor = Color(0xFF2CFF8C)
+val mainBorderColor = Color(0xFFFFFFFF)
+
+
+
+
+val BTCColor = Color(0xFFE7953D)
+val PPCColor = Color(0xFF00B346)
+val ETHColor = Color(0xFF6E84FF)
+
+
+
+val mainBGCDark = Color(0xFF292E3F)
+val secondaryDark = Color(0xFF323748)
+val mainfontDark = Color(0xFFFFFFFF)
+val selectedUpBTNDark = Color(0xFF4B5166)
+val unSelectedDownBTNDark = Color(0xFF4B5166)
+val graphBlockDark = Color(0xFF292D3B)
+
+
+val mainBGCLight = Color(0xFFF4F6FC)
+val secondaryLight = Color(0xFFF7F9FF)
+val mainfontLight = Color(0xFF292E3F)
+val selectedUpBTNLight = Color(0xFF4B5166)
+val unSelectedDownBTNLight = Color(0xFFDBE0F3)
+val graphBlockLight = Color(0xFFDBE0F3)
